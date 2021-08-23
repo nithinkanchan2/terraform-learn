32729945e3498f147780d2cef79689b077e4f1c0
nithinkanchan2 <89379606+nithinkanchan2@users.noreply.github.com>
2021-08-23 14:43:32 +0530

    Create main.tf

---
+++ demo1/main.tf
@@ -0,0 +1,35 @@
+terraform {
+ required_providers {
+   aws = {
+     source = "hashicorp/aws"
+     version = "~>3.27"
+   }
+ }
+  require_version = ">=0.14.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-2"
+}
+
+resource "aws_instance" "app-server" {
+  ami = "ami-0bff25b43a4479334"
+  instance_type = "t2.micro"
+  
+  tags {
+    name = "nithin"
+    environment = "test"
+  }    
+}
+
+
+
+
+
+
+
+
+
+
+
