fc14fed4c2c17ea408447b50bc8f5540106bfa9c
nithinkanchan2 <89379606+nithinkanchan2@users.noreply.github.com>
2021-08-23 16:22:42 +0530

    Update main.tf

--- demo1/main.tf
+++ demo1/main.tf
@@ -13,12 +13,12 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "app-server" {
+resource "aws_instance" "app-server1" {
   ami = "ami-01ebc0c10725bf4b7"
   instance_type = "t2.micro"
   
   tags = {
-   Name = "nithin"
+    Name = "nithin"
   #   = "nithin"
     #environment = "test"
   }    
