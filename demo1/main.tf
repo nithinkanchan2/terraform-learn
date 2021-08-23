2fb7a25c471b6dcaa03b2814a3a3b860a2bc39a8
nithinkanchan2 <89379606+nithinkanchan2@users.noreply.github.com>
2021-08-23 15:12:46 +0530

    Update main.tf

--- demo1/main.tf
+++ demo1/main.tf
@@ -17,9 +17,9 @@ resource "aws_instance" "app-server" {
   ami = "ami-0bff25b43a4479334"
   instance_type = "t2.micro"
   
-  tags {
+  tag {
     name = "nithin"
-    environment = "test"
+    #environment = "test"
   }    
 }
 
