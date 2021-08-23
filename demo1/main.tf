ad4e720741da66b4beebd4d6c59ab17886cbc487
nithinkanchan2 <89379606+nithinkanchan2@users.noreply.github.com>
2021-08-23 16:46:26 +0530

    Update main.tf

--- demo1/main.tf
+++ demo1/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "app-server1" {
   instance_type = "t2.micro"
   
   tags = {
-    Name = "nithin"
+    Name = var.instance_name
   #   = "nithin"
     #environment = "test"
   }    
