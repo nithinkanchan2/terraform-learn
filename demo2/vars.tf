29707fad108c2f741db8bccd0b0f623a421bda26
nithinkanchan2 <89379606+nithinkanchan2@users.noreply.github.com>
2021-08-24 12:20:05 +0530

    Update vars.tf

--- demo2/vars.tf
+++ demo2/vars.tf
@@ -6,11 +6,10 @@ variable "AWS_SECRETE_KEY" {
 }
 variable "AWS_REGION" {
     default = "us-east-2"
-
+}
 variable "AMIS" {
   type = map(string)
   default = {
     us-east-2 = "ami-08d70e59c07c61a3a"
   }
-}
 }
