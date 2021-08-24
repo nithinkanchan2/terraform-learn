bc83ec212742c2f5e96676f7c3717eb1702e4a10
nithinkanchan2 <89379606+nithinkanchan2@users.noreply.github.com>
2021-08-24 09:32:36 +0530

    Update output.tf

--- demo1/output.tf
+++ demo1/output.tf
@@ -1,11 +1,11 @@
 output "instance_id" {
   description = "Instance ID is"
-  value = "aws_instance.app_server.id"
+  value = "aws_instance.app_server1.id"
 }
 
 output "public_ip" {
   description = "public IP is"
-  value = "aws_instance.app_server.public_ip"
+  value = "aws_instance.app_server1.public_ip"
 }
 
 
