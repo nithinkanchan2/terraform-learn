5dc2e6fa1036221c8dfbe8ca15eddc8eefd7a497
nithinkanchan2 <89379606+nithinkanchan2@users.noreply.github.com>
2021-08-24 09:45:19 +0530

    Create main.tf

---
+++ demo2/main.tf
@@ -0,0 +1,32 @@
+terraform {
+ required_providers {
+   aws = {
+     source = "hashicorp/aws"
+     version = "~>3.27"
+   }
+ }
+  #require_version = "=>0.14.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "demo"{
+  bucket = "demo_bucket"
+  acl = "private"
+}
+
+tags {
+  
+}
+
+
+
+
+
+
+
+
+
